bffcd198b3f2364b1c52a722d2009317b4ee5c9e
Anton Bobrov <anthony020@yandex.ru>
2023-04-07 18:30:11 +0300

    Fix:

--- src/main/java/ru/yandex/practicum/filmorate/storage/dal/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dal/FilmStorage.java
@@ -6,8 +6,11 @@ import java.util.Collection;
 public interface FilmStorage {
 
     Film postFilm(Film film);
+
     Film updateFilm(Film film);
+
     Film getFilmById(long id);
+
     Collection<Film> getAllFilms();
 }
 
